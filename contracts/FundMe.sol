101bf66878cd2640aaf4cb4edf6a7b93c525b6eb
cloudmate404 <ogunniyi95@gmail.com>
2022-03-07 12:50:47 +0100

    initial commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,73 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.6 <0.9.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        //$50
+        uint256 minimumUSD = 5 * 10**18;
+        require(getConversionRate(msg.value) >= minimumUSD, "Minimum of $50");
+
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        return uint256(answer * 10**10);
+    }
+
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUsd;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Youre not owner");
+        _;
+    }
+
+    //Withdraw Function
+    function withdraw() public payable onlyOwner {
+        // only want the contract admin/owner to withdraw
+        // require msg.sender = owner
+        payable(msg.sender).transfer(address(this).balance);
+
+        // To reset everyone's account to zero after withdrawal
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // To reset the fundersArray
+        funders = new address[](0);
+    }
+}
